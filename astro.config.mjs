84a14d4656ac1afef06d3dffa46500340b2968de
Sebastian Leal <sebastianleal2016@gmail.com>
2024-01-15 22:04:03 -0500

    [DEPLOY] deploy at vercel configuration 1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });
